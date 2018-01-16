7c738cca52a5685fa382591cf8c3977c99be6f53
AJ Ansel <andrew.ansel6@gmail.com>
2018-01-16 11:44:55 -0800

    Make homescreen a list of all sets that navigate to the 'show' for each set on value change

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import {StackNavigator} from 'react-navigation';
 import React from 'react';
 import { StyleSheet, Text, View, FlatList, ScrollView, Alert } from 'react-native';
 import Setscreen from './Setscreen';
+import Homescreen from './Homescreen';
 import Card from './Card';
 
 const MTGApp = StackNavigator({
+  Home: { screen: Homescreen },
   Set: { screen: Setscreen },
   Card: { screen: Card },
 });
